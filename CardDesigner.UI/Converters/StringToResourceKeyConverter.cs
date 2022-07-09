0a4f2ded819f650cad30657bb1d97cce57cc538e
Rok Goljat <rok.goljat@gmail.com>
2022-07-09 18:39:41 +0200

    #D - Created String to resource key converter

---
+++ CardDesigner.UI/Converters/StringToResourceKeyConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CardDesigner.UI.Converters
+{
+    public class StringToResourceKeyConverter : MarkupExtension, IValueConverter
+    {
+        public string Formatter { get; set; } = string.Empty;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string resourceKey;
+
+            if (Formatter != string.Empty)
+            {
+                resourceKey = Formatter.Replace("(I)", value.ToString());
+            }
+            else
+            {
+                resourceKey = value.ToString();
+            }
+
+            return Application.Current.Resources[resourceKey];
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
