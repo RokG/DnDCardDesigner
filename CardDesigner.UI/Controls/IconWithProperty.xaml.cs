588aca230d4545fcea7ff6419530ae847018b2a2
Rok Goljat <rok.goljat@gmail.com>
2022-07-19 21:57:16 +0200

    #D - Updated CardView and Icons

---
+++ CardDesigner.UI/Controls/IconWithProperty.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardDesigner.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for IconWithProperty.xaml
+    /// </summary>
+    public partial class IconWithPropertyControl : UserControl
+    {
+        public IconWithPropertyControl()
+        {
+            InitializeComponent();
+        }
+
+        public object Icon
+        {
+            get { return (object)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(object), typeof(IconWithPropertyControl), new PropertyMetadata(null));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(IconWithPropertyControl), new PropertyMetadata(0.0));
+
+        public string Unit
+        {
+            get { return (string)GetValue(UnitProperty); }
+            set { SetValue(UnitProperty, value); }
+        }
+
+        public static readonly DependencyProperty UnitProperty =
+            DependencyProperty.Register("Unit", typeof(string), typeof(IconWithPropertyControl), new PropertyMetadata(string.Empty));
+
+    }
+}
