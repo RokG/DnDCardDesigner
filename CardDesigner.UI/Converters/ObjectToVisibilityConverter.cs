d5eefc0ea4266cc587fc5c63c0067029cdcd7a5a
Rok Goljat <rok.goljat@gmail.com>
2022-07-25 20:28:47 +0200

    #D - Implemented main window style

---
+++ CardDesigner.UI/Converters/ObjectToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CardDesigner.UI.Converters
+{
+    public class ObjectToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ CardDesigner.UI/Styles/WindowStyles.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace CardDesigner.UI
+{
+    public partial class WindowStyles : ResourceDictionary
+    {
+        public WindowStyles()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs eventArgs)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void OnMinimizeClick(object sender, RoutedEventArgs eventArgs)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+
+        }
+
+        private void OnMaximizeRestoreClick(object sender, RoutedEventArgs eventArgs)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+    }
+}
