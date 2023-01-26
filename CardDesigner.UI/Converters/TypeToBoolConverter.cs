86238fe5ff119a5432774eed17ab273c84c2beef
Rok Goljat <rok.goljat@gmail.com>
2023-01-26 20:43:28 +0100

    #3 - Fixed XAML binding issues

---
+++ CardDesigner.UI/Converters/TypeToBoolConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CardDesigner.UI.Converters
+{
+
+
+    public class TypeToStringConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "null";
+            }
+            else
+            {
+                return value.GetType().Name;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+    /// This converter is needed because these two things are not working:
+
+    /// This is throwing binding failures when switching type, although everything works
+    //<ContentControl>
+    //    <ContentControl.Style>
+    //        <Style TargetType = "{x:Type ContentControl}" >
+    //            < Style.Triggers >
+    //                < DataTrigger
+    //                    Binding="{Binding ItemCard.Type, ElementName=itemCard, Mode=OneWay}"
+    //                    Value="{x:Static enums:ItemType.Armour}">
+    //                    <Setter Property = "Template" Value="{StaticResource ArmorTemplate}" />
+    //                </DataTrigger>
+    //                <DataTrigger
+    //                    Binding = "{Binding ItemCard.Type, ElementName=itemCard, Mode=OneWay}"
+    //                    Value="{x:Static enums:ItemType.Weapon}">
+    //                    <Setter Property = "Template" Value="{StaticResource WeaponTemplate}" />
+    //                </DataTrigger>
+    //            </Style.Triggers>
+    //        </Style>
+    //    </ContentControl.Style>
+    //</ContentControl>
+
+    /// This is not getting triggered, altho the binding seems to be Class and not INterface when checking with converter
+    // <ContentControl Margin = "5,0" >
+    //    < ContentControl.Style >
+    //        < Style TargetType="{x:Type ContentControl}">
+    //            <Style.Triggers>
+    //                <DataTrigger
+    //                    Binding = "{Binding SelectedItemCard.Item}"
+    //                    Value="{x:Type models:ArmourModel}">
+    //                    <Setter Property = "Template" Value="{StaticResource ArmorTemplate}" />
+    //                </DataTrigger>
+    //                <DataTrigger
+    //                    Binding = "{Binding SelectedItemCard.Item}"
+    //                    Value="{x:Type models:WeaponModel}">
+    //                    <Setter Property = "Template" Value="{StaticResource WeaponTemplate}" />
+    //                </DataTrigger>
+    //            </Style.Triggers>
+    //        </Style>
+    //    </ContentControl.Style>
+    //</ContentControl>
+}
