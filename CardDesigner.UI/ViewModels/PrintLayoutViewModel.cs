6dbd21adf90fba700b4b77f32848c545a6a7292d
Rok Goljat <rok.goljat@gmail.com>
2023-02-24 21:45:48 +0100

    #18 - Successfully printed PDF page

--- CardDesigner.UI/ViewModels/PrintLayoutViewModel.cs
+++ CardDesigner.UI/ViewModels/PrintLayoutViewModel.cs
@@ -157,6 +157,5 @@ namespace CardDesigner.UI.ViewModels
         }
 
         #endregion
-
     }
 }
--- CardDesigner.UI/Views/PrintLayoutView.xaml.cs
+++ CardDesigner.UI/Views/PrintLayoutView.xaml.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Packaging;
+using System.IO;
+using System.Printing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Xps.Packaging;
+using System.Windows.Xps;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CardDesigner.Domain.Enums;
+using Microsoft.Win32;
+using System.Xml.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Drawing.Printing;
 
 namespace CardDesigner.UI.Views
 {
@@ -23,6 +26,38 @@ namespace CardDesigner.UI.Views
         public PrintLayoutView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //// Printa v kvadrat?
+            /*
+            *  Convert WPF -> XPS -> PDF
+            */
+            MemoryStream lMemoryStream = new MemoryStream();
+            Package package = Package.Open(lMemoryStream, FileMode.Create);
+            XpsDocument doc = new XpsDocument(package);
+            XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
+
+            // This is your window
+            writer.Write(itemCardsToPDF);
+
+            doc.Close();
+            package.Close();
+
+            // Convert 
+            MemoryStream outStream = new MemoryStream();
+            PdfSharp.Xps.XpsConverter.Convert(lMemoryStream, outStream, false);
+
+            // Write pdf file
+            FileStream fileStream = new FileStream("E:\\test1" +DateTime.Now.ToString("yyyy_mm_dd_hh_mm_ss")+ ".pdf", FileMode.Create);
+            outStream.CopyTo(fileStream);
+
+            // Clean up
+            outStream.Flush();
+            outStream.Close();
+            fileStream.Flush();
+            fileStream.Close();
         }
     }
 }
