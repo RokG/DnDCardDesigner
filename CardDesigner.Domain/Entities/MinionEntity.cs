e6f2322491542687a8161a3e04a52ad6b93aea26
Rok Goljat <rok.goljat@gmail.com>
2023-02-27 00:10:24 +0100

    #19 - Updated Minion Properties

--- CardDesigner.Domain/Entities/MinionEntity.cs
+++ CardDesigner.Domain/Entities/MinionEntity.cs
@@ -11,17 +11,28 @@ namespace CardDesigner.Domain.Entities
         // Properties
         public string Name { get; set; }
         public string Title { get; set; }
-        //public CharacterAbilitiesEntity Abilities { get; set; }
-        public Race Race { get; set; }
         public Alignment Alignment { get; set; }
+        public string Appearance { get; set; } // Small humanoid
         public int Hitpoints { get; set; }
         public int ArmourClass { get; set; }
-        public int FlyingSpeed { get; set; }
         public int Speed { get; set; }
+        public int FlyingSpeed { get; set; }
         public int ClimbingSpeed { get; set; }
         public int SwimingSpeed { get; set; }
         public int Initiative { get; set; }
         public int PassivePerception { get; set; }
-        public int PassiveInsight { get; set; }
+
+        //https://www.aidedd.org/dnd/monstres.php?vo=bullywug
+        public string Abilities { get; set; } // Parse STR,DEX..
+        public string Attributes { get; set; } // Better name? - Description 1
+        public string Actions { get; set; } // Descriptions of attacks - Description 2
+        public string SavingThrows { get; set; }
+        public string Skills { get; set; }
+        public string Senses { get; set; }
+        public string Languages { get; set; }
+        public string ChalangeRating { get; set; }
+        public string DamageResistances { get; set; }
+        public string DamageImmunities { get; set; }
+        public string ConditionImmunities { get; set; }
     }
 }
