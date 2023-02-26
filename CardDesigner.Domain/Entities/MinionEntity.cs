6a40961ddd15b82420fb574d44038d7dd67fa47b
Rok Goljat <rok.goljat@gmail.com>
2023-02-26 23:11:15 +0100

    #19 - Implemented basic Minion View

--- CardDesigner.Domain/Entities/MinionEntity.cs
+++ CardDesigner.Domain/Entities/MinionEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CardDesigner.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace CardDesigner.Domain.Entities
 {
@@ -10,5 +11,17 @@ namespace CardDesigner.Domain.Entities
         // Properties
         public string Name { get; set; }
         public string Title { get; set; }
+        //public CharacterAbilitiesEntity Abilities { get; set; }
+        public Race Race { get; set; }
+        public Alignment Alignment { get; set; }
+        public int Hitpoints { get; set; }
+        public int ArmourClass { get; set; }
+        public int FlyingSpeed { get; set; }
+        public int Speed { get; set; }
+        public int ClimbingSpeed { get; set; }
+        public int SwimingSpeed { get; set; }
+        public int Initiative { get; set; }
+        public int PassivePerception { get; set; }
+        public int PassiveInsight { get; set; }
     }
 }
--- CardDesigner.Domain/Models/MinionModel.cs
+++ CardDesigner.Domain/Models/MinionModel.cs
@@ -1,11 +1,26 @@
-﻿namespace CardDesigner.Domain.Models
+﻿using CardDesigner.Domain.Enums;
+using CardDesigner.Domain.Interfaces;
+
+namespace CardDesigner.Domain.Models
 {
-    public class MinionModel
+    public class MinionModel : ISelectableItem
     {
         public int ID { get; set; }
 
         // Properties
         public string Name { get; set; }
         public string Title { get; set; }
+        //public CharacterAbilitiesModel Abilities { get; set; }
+        public Race Race { get; set; }
+        public Alignment Alignment { get; set; }
+        public int Hitpoints { get; set; }
+        public int ArmourClass { get; set; }
+        public int FlyingSpeed { get; set; }
+        public int Speed { get; set; }
+        public int ClimbingSpeed { get; set; }
+        public int SwimingSpeed { get; set; }
+        public int Initiative { get; set; }
+        public int PassivePerception { get; set; }
+        public int PassiveInsight { get; set; }
     }
 }
