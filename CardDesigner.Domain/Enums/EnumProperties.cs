e8993bb7be2e9d0141ae19b573566dcbfa3bade6
Rok Goljat <rok.goljat@gmail.com>
2023-03-06 20:09:22 +0100

    #20 - Resolved messages

--- CardDesigner.Domain/Enums/EnumProperties.cs
+++ CardDesigner.Domain/Enums/EnumProperties.cs
@@ -6,63 +6,45 @@
         {
             if (enumObject is RangeType rangeType)
             {
-                switch (rangeType)
+                return rangeType switch
                 {
-                    case RangeType.Self:
-                        return new("Self", 0, 0, false);
-                    case RangeType.Touch:
-                        return new("Tch", 0, 0, false);
-                    case RangeType.Distance:
-                        return new("ft", 0, 100, true);
-                    default:
-                        return new(string.Empty, -1, -1, false);
-                }
+                    RangeType.Self => new("Self", 0, 0, false),
+                    RangeType.Touch => new("Tch", 0, 0, false),
+                    RangeType.Distance => new("ft", 0, 100, true),
+                    _ => new(string.Empty, -1, -1, false),
+                };
             }
             else if (enumObject is CastingTimeType castingTimeType)
             {
-                switch (castingTimeType)
+                return castingTimeType switch
                 {
-                    case CastingTimeType.Action:
-                        return new("A", 0, 0, false);
-                    case CastingTimeType.BonusAction:
-                        return new("BA", 0, 0, false);
-                    case CastingTimeType.Reaction:
-                        return new("R", 0, 0, false);
-                    case CastingTimeType.Second:
-                        return new("sec", 0, 60, true);
-                    case CastingTimeType.Minute:
-                        return new("min", 0, 60, true);
-                    case CastingTimeType.Hour:
-                        return new("h", 0, 24, true);
-                    case CastingTimeType.Day:
-                        return new("day", 0, 7, true);
-                    default:
-                        return new(string.Empty, -1, -1, false);
-                }
+                    CastingTimeType.Action => new("A", 0, 0, false),
+                    CastingTimeType.BonusAction => new("BA", 0, 0, false),
+                    CastingTimeType.Reaction => new("R", 0, 0, false),
+                    CastingTimeType.Second => new("sec", 0, 60, true),
+                    CastingTimeType.Minute => new("min", 0, 60, true),
+                    CastingTimeType.Hour => new("h", 0, 24, true),
+                    CastingTimeType.Day => new("day", 0, 7, true),
+                    _ => new(string.Empty, -1, -1, false),
+                };
             }
             else if (enumObject is DurationType durationType)
             {
-                switch (durationType)
+                return durationType switch
                 {
-                    case DurationType.Instantaneous:
-                        return new("Ins.", 0, 0, false);
-                    case DurationType.Second:
-                        return new("sec", 1, 60, true);
-                    case DurationType.Minute:
-                        return new("min", 1, 60, true);
-                    case DurationType.Hour:
-                        return new("h", 1, 24, true);
-                    case DurationType.Day:
-                        return new("day", 1, 7, true);
-                    default:
-                        return new(string.Empty, -1, -1, false);
-                }
+                    DurationType.Instantaneous => new("Ins.", 0, 0, false),
+                    DurationType.Second => new("sec", 1, 60, true),
+                    DurationType.Minute => new("min", 1, 60, true),
+                    DurationType.Hour => new("h", 1, 24, true),
+                    DurationType.Day => new("day", 1, 7, true),
+                    _ => new(string.Empty, -1, -1, false),
+                };
             }
-            else if (enumObject is AreaOfEffect areaOfEffect)
+            else if (enumObject is AreaOfEffect)
             {
                 return new("ft", 0, 100, true);
             }
-            else if (enumObject is MagicDamageType damageType)
+            else if (enumObject is MagicDamageType)
             {
                 return new(string.Empty, 0, 10, false);
             }
@@ -79,17 +61,13 @@
             }
             else if (enumObject is TargetType targetType)
             {
-                switch (targetType)
+                return targetType switch
                 {
-                    case TargetType.Self:
-                        return new("N/A", 0, 0, false);
-                    case TargetType.Touch:
-                        return new("N/A", 0, 0, false);
-                    case TargetType.Target:
-                        return new("ft", 0, 1000, true);
-                    default:
-                        return new(string.Empty, -1, -1, false);
-                }
+                    TargetType.Self => new("N/A", 0, 0, false),
+                    TargetType.Touch => new("N/A", 0, 0, false),
+                    TargetType.Target => new("ft", 0, 1000, true),
+                    _ => new(string.Empty, -1, -1, false),
+                };
             }
             else
             {
