7c41709b2af04d4967c5058b0db62268836de90e
Rok Goljat <rok.goljat@gmail.com>
2023-03-07 22:21:47 +0100

    #20 - Character Deck Update methods

--- CardDesigner.Domain/Models/CharacterModel.cs
+++ CardDesigner.Domain/Models/CharacterModel.cs
@@ -16,7 +16,7 @@ namespace CardDesigner.Domain.Models
         public List<SpellDeckDesignLinkerModel> SpellDeckDescriptors { get; set; } = new();
         public List<ItemDeckDesignLinkerModel> ItemDeckDescriptors { get; set; } = new();
         public List<CharacterDeckDesignLinkerModel> CharacterDeckDescriptors { get; set; } = new();
-        public List<MinionDeckDesignLinkerModel> MinionDeckDescriptors { get; set; }
+        public List<MinionDeckDesignLinkerModel> MinionDeckDescriptors { get; set; } = new();
         public DeckBackgroundDesignModel DeckBackgroundDesign { get; set; } = new();
         public List<CharacterClassModel> Classes { get; set; } = new();
         public CharacterAbilitiesModel Abilities { get; set; } = new();
